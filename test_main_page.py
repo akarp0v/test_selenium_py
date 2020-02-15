1f36d35d4880a84a9a29ae1366d717e86e1ae1af
akarp0v <komhleb@yandex.ru>
2020-02-15 16:28:16 +0300

    main page test is almost Page Object styled

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,11 @@
+from .pages.main_page import MainPage
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)
+    # открываем страницу
+    page.open()
+    # выполняем метод страницы - переходим на страницу логина
+    page.go_to_login_page()
