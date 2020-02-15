a10776c497563d520e03173793466fca910510be
akarp0v <komhleb@yandex.ru>
2020-02-15 17:06:30 +0300

    test main page updated: login_link verification added

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,20 @@
 from .pages.main_page import MainPage
 
 
+# Открыть главную страницу
+# Перейти на страницу логина
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page = MainPage(browser, link)
-    # открываем страницу
     page.open()
-    # выполняем метод страницы - переходим на страницу логина
     page.go_to_login_page()
+
+
+# Открыть главную страницу
+# Проверить, что есть ссылка, которая ведет на логин
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
